17e7e06c13722f37b0d920bba0736c2f54262c1a
lawrence788 <59813147+lawrence788@users.noreply.github.com>
2020-05-27 11:44:01 -0700

    updated with parts 3 and 4, verified part 8

--- lab3a.c
+++ lab3a.c
@@ -2,6 +2,8 @@
 // EMAIL: maguanqun0212@gmail.com
 // ID: 305331164
 
+//3,4,7,8 for Larry
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -11,9 +13,79 @@
 #include <time.h>  
 #include "ext2_fs.h"
 
+
 int image_fd;
 struct ext2_super_block superBLK;
 unsigned int block_size;
+
+
+void FreeBlockEntries(){
+	//1 means block is used, 0 means block is free
+	struct ext2_group_desc groupBLK;
+    
+	//SUPERBLOCK,1024,1000,1024,128,8192,1000,11
+	//GROUP,0,1024,1000,513,567,3,4,5	
+
+	//we are only working on single group
+    	off_t offset = 1024 + block_size; //offset for superblock + superblock's size to get the offset for groups
+    	pread(image_fd, &groupBLK, 32, offset); //size of the block group descriptor structure is 32 bytes
+ 	
+	//block where bitmap is stored
+	int bitmap_loc = groupBLK.bg_block_bitmap;
+		   	
+	//offset of bitmap
+	unsigned char bitmap[1024] = {0};
+	off_t bitmap_offset = 1024 + (bitmap_loc-1)*block_size; //bitmap is block 3, which is 2 blocks after superblock
+	pread(image_fd, &bitmap, 128, bitmap_offset); //size of bitmap is numberofblocks/8 bytes long
+
+	//traverse the bitmap and print the block number if the bitmap bit is 0(free)
+	int i=0;
+	for(; i<128; i++){
+		int j=0;
+		for(; j<8; j++){
+			if( ((bitmap[i] >> j) & 1) == 0){ //if the bit is 0
+				dprintf(STDOUT_FILENO, "BREE,%d\n", i*8+j+1);
+			}
+		}
+	}
+}
+
+
+void FreeInodeEntries(){
+        //1 means block is used, 0 means block is free
+        struct ext2_group_desc groupBLK;
+
+        //SUPERBLOCK,1024,1000,1024,128,8192,1000,11
+        //GROUP,0,1024,1000,513,567,3,4,5
+	//inode numbers go from 1 to 1000
+
+        //we are only working on single group
+        off_t offset = 1024 + block_size; //offset for superblock + superblock's size to get the offset for groups
+        pread(image_fd, &groupBLK, 32, offset); //size of the block group descriptor structure is 32 bytes
+
+        //block where bitmap is stored
+        int bitmap_loc = groupBLK.bg_inode_bitmap;
+
+	//number of inodes starting from 1
+	int num_inodes = superBLK.s_inodes_per_group;
+
+        //offset of bitmap
+        unsigned char bitmap[1024] = {0};
+        off_t bitmap_offset = 1024 + (bitmap_loc-1)*block_size; //bitmap is block 4, which is 3 blocks after superblock
+        pread(image_fd, &bitmap, num_inodes/8, bitmap_offset); //size of bitmap is numberofblocks/8 bytes long
+
+        //traverse the bitmap and print the inode number if the bitmap bit is 0(free)
+        int i=0;
+        for(; i<num_inodes/8; i++){
+                int j=0;
+                for(; j<8; j++){
+                        if( ((bitmap[i] >> j) & 1) == 0){ //if the bit is 0
+                                dprintf(STDOUT_FILENO, "IREE,%d\n", i*8+j+1);
+                        }
+                }
+        }
+}
+
 
 void SuperblockSummary()
 {
@@ -44,7 +116,8 @@ void InodeSummary()
     struct ext2_inode* inode_id = (struct ext2_inode*)malloc(num_inodes * sizeof(struct ext2_inode));
     off_t offset = 1024 + 4 * block_size; //offset for superblock + 4 blocks whose size is "block_size"
     size_t inode_size = sizeof(struct ext2_inode);
-    for(__u32 i = 0; i < num_inodes; i++)
+    __u32 i = 0;
+    for(; i < num_inodes; i++)
     {
         pread(image_fd, &inode_id[i], inode_size, offset + i * inode_size);
 
@@ -89,7 +162,8 @@ void InodeSummary()
         
         if((file_type != 's') || (file_type == 's' && inode_id[i].i_blocks != 0))
         {
-            for(int j = 0; j < 15; j++)
+     	    int j = 0;
+	    for(; j < 15; j++)
             {
                 dprintf(STDOUT_FILENO, ",%d", inode_id[i].i_block[j]);
             }
@@ -99,7 +173,8 @@ void InodeSummary()
         //directory entries
         if(file_type == 'd')
         {
-            for(int k = 0; k < EXT2_NDIR_BLOCKS; k++) //first 12 are direct blocks
+           int k = 0; 
+	   for(; k < EXT2_NDIR_BLOCKS; k++) //first 12 are direct blocks
             {
                 if(inode_id[i].i_block[k] != 0) //directory entry is not empty
                 {
@@ -123,6 +198,7 @@ void InodeSummary()
     }
 }
 
+
 int main(int argc, char *argv[])
 {
     if(argc != 2)
@@ -141,6 +217,8 @@ int main(int argc, char *argv[])
 
     SuperblockSummary();
     GroupSummary();
+    FreeBlockEntries();
+    FreeInodeEntries();
     InodeSummary();
 
     return 0;
